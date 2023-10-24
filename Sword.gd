c40053903b1f61e34851e5d7084f696bfa7e0254
Nevarwel <adantenoel06@gmail.com>
2023-10-24 21:42:50 +0800

    added tilemaps, camera
    ready to publish as proof of concept

--- Sword.gd
+++ Sword.gd
@@ -5,38 +5,36 @@ var orientation = "right"
 
 
 func _process(_delta):
-	var clockwise = true
-	
-	if Input.is_key_pressed(KEY_SHIFT):
-		clockwise = false
+
+	var clockwise = false if Input.is_key_pressed(KEY_SPACE) else true
 
 	if !rotating:
-		if Input.is_action_just_pressed("ui_right"):
+		if Input.is_action_just_pressed("right"):
 			rotate_sword(180, 0, clockwise, "right")
 
-		if Input.is_action_just_pressed("ui_down"):
+		elif Input.is_action_just_pressed("down"):
 			rotate_sword(270, 90, clockwise, "down")
 
-		if Input.is_action_just_pressed("ui_left"):
+		elif Input.is_action_just_pressed("left"):
 			rotate_sword(0, 180, clockwise, "left")
 
-		if Input.is_action_just_pressed("ui_up"):
+		elif Input.is_action_just_pressed("up"):
 			rotate_sword(90, 270, clockwise, "up")
 
 
-func rotate_sword(rotation_if_same: int, rotation_if_different: int, _clockwise: bool, _orientation: String):
+func rotate_sword(rotation_if_same: int, rotation_if_different: int, clockwise: bool, to_orientation: String):
 	rotation_degrees = wrapf(rotation_degrees, 0, 360)
 	
-	var rotation_value = rotation_if_same if (_orientation == orientation) else rotation_if_different
+	var rotation_value = rotation_if_same if (to_orientation == orientation) else rotation_if_different
 
-	if _clockwise == true:
+	if clockwise == true:
 		rotation_value += 360 if (rotation_degrees > rotation_value) else 0
-	elif _clockwise == false:
+	elif clockwise == false:
 		rotation_value -= 360 if (rotation_degrees < rotation_value) else 0
 
 	var target_rotation = rotation_degrees + rotation_value - (int(rotation_degrees) % 360)
 
-	if _orientation == orientation:
+	if to_orientation == orientation:
 		match(orientation):
 			"right":
 				handle_rotation(target_rotation, Vector2(800, 0), "left", 1)
@@ -47,7 +45,7 @@ func rotate_sword(rotation_if_same: int, rotation_if_different: int, _clockwise:
 			"up":
 				handle_rotation(target_rotation, Vector2(0, -800), "down", 1)
 	else:
-		handle_rotation(target_rotation, Vector2(0, 0), _orientation, 0)
+		handle_rotation(target_rotation, Vector2.ZERO, to_orientation, 0)
 
 func handle_rotation(target_rotation: int, position_value: Vector2, orientation_value: String, move: bool):
 	var delay = 0.3
