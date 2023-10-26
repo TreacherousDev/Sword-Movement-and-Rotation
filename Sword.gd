4a9713091f4b61164029c3ef805f2c1c72aad7ef
Nevarwel <adantenoel06@gmail.com>
2023-10-26 22:43:54 +0800

    revamped rotation controls
    clockwise to right hand controls, counterclockwise to left

--- Sword.gd
+++ Sword.gd
@@ -8,34 +8,43 @@ func _ready():
 	offset.x = sword_size / 2
 
 func _process(_delta):
-
 	var clockwise = false if Input.is_key_pressed(KEY_SPACE) else true
-
 	if !rotating:
-		if Input.is_action_just_pressed("right"):
-			rotate_sword(180, 0, clockwise, "right")
+		check_input()
 
-		elif Input.is_action_just_pressed("down"):
-			rotate_sword(270, 90, clockwise, "down")
 
-		elif Input.is_action_just_pressed("left"):
-			rotate_sword(0, 180, clockwise, "left")
+func check_input():
+	if Input.is_action_just_pressed("right"):
+		rotate_sword(180, 0, false, "right")
+	elif Input.is_action_just_pressed("down"):
+		rotate_sword(270, 90, false, "down")
+	elif Input.is_action_just_pressed("left"):
+		rotate_sword(0, 180, false, "left")
+	elif Input.is_action_just_pressed("up"):
+		rotate_sword(90, 270, false, "up")
 
-		elif Input.is_action_just_pressed("up"):
-			rotate_sword(90, 270, clockwise, "up")
+	elif Input.is_action_just_pressed("ui_right"):
+		rotate_sword(180, 0, true, "right")
+	elif Input.is_action_just_pressed("ui_down"):
+		rotate_sword(270, 90, true, "down")
+	elif Input.is_action_just_pressed("ui_left"):
+		rotate_sword(0, 180, true, "left")
+	elif Input.is_action_just_pressed("ui_up"):
+		rotate_sword(90, 270, true, "up")
 
 
 func rotate_sword(to_rotation_if_same: int, to_rotation_if_different: int, clockwise: bool, to_orientation: String):
 	rotation_degrees = wrapf(rotation_degrees, 0, 360)
 	
-	var rotation_value = to_rotation_if_same if (to_orientation == orientation) else to_rotation_if_different
+	var target_rotation = to_rotation_if_same if (to_orientation == orientation) else to_rotation_if_different
 
+	#add / subtract 1 full rotation so interpolation happens in the proper direction
+	#ex: 270 to 90 clockwise will be 270 to 450, since negatively incrementing will be ccw rotation
+	#rotation will be wrapped back to 0-360 on the next rotation call
 	if clockwise == true:
-		rotation_value += 360 if (rotation_degrees > rotation_value) else 0
+		target_rotation += 360 if (rotation_degrees > target_rotation) else 0
 	elif clockwise == false:
-		rotation_value -= 360 if (rotation_degrees < rotation_value) else 0
-
-	var target_rotation = rotation_degrees + rotation_value - (int(rotation_degrees) % 360)
+		target_rotation -= 360 if (rotation_degrees < target_rotation) else 0
 	
 	var move_direction := Vector2.ZERO
 	var move: bool = to_orientation == orientation
@@ -58,7 +67,7 @@ func rotate_sword(to_rotation_if_same: int, to_rotation_if_different: int, clock
 	handle_rotation(target_rotation, move_direction, to_orientation, move)
 
 func handle_rotation(target_rotation: int, position_value: Vector2, orientation_value: String, move: bool):
-	var delay = 0.3
+	var delay = 0.15
 	rotating = true
 	
 	if move == true:
@@ -66,7 +75,7 @@ func handle_rotation(target_rotation: int, position_value: Vector2, orientation_
 		global_position += position_value
 
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "rotation_degrees", target_rotation, delay).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN)
+	tween.tween_property(self, "rotation_degrees", target_rotation, delay).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
 	await get_tree().create_timer(delay).timeout
 	
 	if move == true:
