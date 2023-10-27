2f0af76ec4675a9ddb15ac8f2c06be73f3c8f636
Nevarwel <adantenoel06@gmail.com>
2023-10-27 22:38:45 +0800

    removed unnecessary bits

--- Sword.gd
+++ Sword.gd
@@ -2,13 +2,13 @@ extends Sprite2D
 
 var rotating = false
 var orientation = "right"
+var delay = 0.15
 @onready var sword_size : int = texture.get_width()
 
 func _ready():
 	offset.x = sword_size / 2
 
 func _process(_delta):
-	var clockwise = false if Input.is_key_pressed(KEY_SPACE) else true
 	if !rotating:
 		check_input()
 
@@ -31,20 +31,20 @@ func check_input():
 		rotate_sword(0, 180, true, "left")
 	elif Input.is_action_just_pressed("ui_up"):
 		rotate_sword(90, 270, true, "up")
+		
+	
 
 
 func rotate_sword(to_rotation_if_same: int, to_rotation_if_different: int, clockwise: bool, to_orientation: String):
-	rotation_degrees = wrapf(rotation_degrees, 0, 360)
-	
 	var target_rotation = to_rotation_if_same if (to_orientation == orientation) else to_rotation_if_different
 
 	#add / subtract 1 full rotation so interpolation happens in the proper direction
 	#ex: 270 to 90 clockwise will be 270 to 450, since negatively incrementing will be ccw rotation
 	#rotation will be wrapped back to 0-360 on the next rotation call
 	if clockwise == true:
-		target_rotation += 360 if (rotation_degrees > target_rotation) else 0
+		rotation_degrees -= 360 if (rotation_degrees > target_rotation) else 0
 	elif clockwise == false:
-		target_rotation -= 360 if (rotation_degrees < target_rotation) else 0
+		rotation_degrees += 360 if (rotation_degrees < target_rotation) else 0
 	
 	var move_direction := Vector2.ZERO
 	var move: bool = to_orientation == orientation
@@ -67,7 +67,6 @@ func rotate_sword(to_rotation_if_same: int, to_rotation_if_different: int, clock
 	handle_rotation(target_rotation, move_direction, to_orientation, move)
 
 func handle_rotation(target_rotation: int, position_value: Vector2, orientation_value: String, move: bool):
-	var delay = 0.15
 	rotating = true
 	
 	if move == true:
@@ -75,8 +74,8 @@ func handle_rotation(target_rotation: int, position_value: Vector2, orientation_
 		global_position += position_value
 
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "rotation_degrees", target_rotation, delay).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
-	await get_tree().create_timer(delay).timeout
+	tween.tween_property(self, "rotation_degrees", target_rotation, delay).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	await tween.finished
 	
 	if move == true:
 		offset.x = (sword_size / 2)
